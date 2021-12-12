d732d8fc0923e8b3c8048fdf8fa06f4c662431de
Seung-Bum <sb910126@gamil.com>
2021-12-13 00:08:06 +0900

    modified modal

--- menu.js
+++ menu.js
@@ -6,6 +6,8 @@ const menus = [
     {name:'Bulgogi Whopper', price:6500, picture:'img/bulgogiWhopper.jpg'}
 ]
 
+
+// 메뉴리스트 생성
 const menuList = document.querySelector("#menuList")
 let str =''
 for (let i = 0; i < menus.length; i++) {
@@ -13,14 +15,15 @@ for (let i = 0; i < menus.length; i++) {
 
     // data-idx값(getAttribute으로 나중에 활용용)을 부여해서주문처리할때 사용
     str += `
-            <div class="card shadow-sm" data-idx="${i}" data-name="${menu.name}" data-price="${menu.price}">
+            <div class="card shadow-sm" data-name="${menu.name}" data-price="${menu.price}">
             <img class="burger" src=${menu.picture}>
             <div class="card-body">
             <p class="card-text">${menu.name}</p>
             <p class="card-text">${menu.price}&#8361;</p>
             <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-outline-secondary">purchase</button>
+                <!-- Trigger the modal with a button -->
+                <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#orderDiv" data-idx="${i}">purchase</button>
                 <button type="button" class="btn btn-sm btn-outline-secondary">information</button>
                 </div>
                 <small class="text-muted" align="right">cooking time<br>10 mins</small>
@@ -30,3 +33,28 @@ for (let i = 0; i < menus.length; i++) {
 
 }
 menuList.innerHTML = str
+
+// 모달창 body
+const modal = document.querySelector("#orderDiv .modal-dialog .modal-content .modal-body")
+
+// 모달창 이벤트 생성
+// 구매버튼
+const purchaseBtn = document.querySelector("#orderDiv")
+purchaseBtn.addEventListener('click', function (e) {
+    let orderStr = `<p>hi</p>`
+    modal.innerHTML = orderStr
+
+    //클릭한 요소값을 가져옴
+    // const target = e.target
+    // console.log("TARGET: " + target)
+
+    // //div의 #menuCard 가까이 클릭
+    // const btnEle = target.closest("#menuCard")
+
+    //상품의 저장되 있던 idx값을 idx변수에 저장
+    const idx = purchaseBtn.getAttribute("data-idx")
+    console.log("IDX: " + idx)
+    
+    
+
+}, false)
