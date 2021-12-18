0fda3a527261c33464bb8e2a269098cb8f99c0db
Seung-Bum <sb910126@gamil.com>
2021-12-19 04:41:13 +0900

    modal event setting

--- menu.js
+++ menu.js
@@ -13,23 +13,23 @@ let str =''
 for (let i = 0; i < menus.length; i++) {
     const menu = menus[i]
 
-    // data-idx값(getAttribute으로 나중에 활용용)을 부여해서주문처리할때 사용
+    // data-idx값(getAttribute으로 나중에 활용)을 부여해서 주문처리할때 사용
     str += `
             <div class="card shadow-sm" data-name="${menu.name}" data-price="${menu.price}">
-            <img class="burger" src=${menu.picture}>
-            <div class="card-body">
-            <p class="card-text">${menu.name}</p>
-            <p class="card-text">${menu.price}&#8361;</p>
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                <!-- Trigger the modal with a button -->
-                <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#orderDiv" data-idx="${i}">purchase</button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">information</button>
+                <img class="burger" src=${menu.picture}>
+                    <div class="card-body">
+                        <p class="card-text">${menu.name}</p>
+                        <p class="card-text">${menu.price}&#8361;</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                            <div id="xBtn-group" class="btn-group" data-idx="${i}">
+                                <!-- Trigger the modal with a button -->
+                                <button type="button" id="purId" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#orderDiv">purchase</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">information</button>
+                            </div>
+                        <small class="text-muted" align="right">cooking time<br>10 mins</small>
+                    </div>
                 </div>
-                <small class="text-muted" align="right">cooking time<br>10 mins</small>
-            </div>
-            </div>
-        </div>`
+            </div>`
 
 }
 menuList.innerHTML = str
@@ -38,23 +38,25 @@ menuList.innerHTML = str
 const modal = document.querySelector("#orderDiv .modal-dialog .modal-content .modal-body")
 
 // 모달창 이벤트 생성
-// 구매버튼
-const purchaseBtn = document.querySelector("#orderDiv")
-purchaseBtn.addEventListener('click', function (e) {
-    let orderStr = `<p>hi</p>`
-    modal.innerHTML = orderStr
+// 구매버튼에서 상품 idx추출
+const xPurBtns = document.querySelectorAll("#purId")
 
-    //클릭한 요소값을 가져옴
-    // const target = e.target
-    // console.log("TARGET: " + target)
+console.log(xPurBtns)
 
-    // //div의 #menuCard 가까이 클릭
-    // const btnEle = target.closest("#menuCard")
 
-    //상품의 저장되 있던 idx값을 idx변수에 저장
-    const idx = purchaseBtn.getAttribute("data-idx")
-    console.log("IDX: " + idx)
-    
-    
+xPurBtns.forEach(purBtn => {
+	purBtn.addEventListener("click", function (e) {
+        console.log("실행")
 
-}, false)
+        //클릭한 요소값을 가져옴
+        const target = e.target
+        console.log("TARGET: " + target)
+
+        const purBtnData = target.closest("#xBtn-group")
+        console.log(purBtnData)
+
+        //상품의 저장되 있던 idx값을 idx변수에 저장
+        const idx = purBtnData.getAttribute("data-idx")
+        console.log("IDX: " + idx)
+    }, false);
+});
